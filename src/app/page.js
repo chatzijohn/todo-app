264bd28178e06a913d4d3643da2a9f6ab4cc683b
pchatzigiannis <pchatzigiannis@technor.gr>
2023-10-19 11:24:09 +0300

    Removed typo in main page

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
 
     return(
         <div>
-           <TitleHeading title="Hello, Welcome to my app." subTitle="I have no clue what I am doing, please help me"  height="50vh" />δοψκ
+           <TitleHeading title="Hello, Welcome to my app." subTitle="I have no clue what I am doing, please help me"  height="50vh" />
         </div>
     )
 }
